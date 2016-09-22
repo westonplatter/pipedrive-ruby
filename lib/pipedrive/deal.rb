76b5b41be5e49d97ee2b4a8ee2b375fc3bf05f21
Mike Szyndel <hajder@users.noreply.github.com>
2016-09-22 16:55:55 +0200

    Add Deal.filter

--- lib/pipedrive/deal.rb
+++ lib/pipedrive/deal.rb
@@ -1,5 +1,21 @@
 module Pipedrive
   class Deal < Base
+    
+    def self.filter(filter_id, fetch_all_pages = true)
+      acc = []
+      start = 0
+      limit = 500
+      loop do
+        response = Deal.get resource_path, query: { filter_id: filter_id, start: start, limit: limit, owned_by_you: 0 }
+        acc.push(*Deal.new_list(response))
+        break unless response['additional_data']['pagination']['more_items_in_collection']
+        break unless fetch_all_pages
+        start += limit
+      end
+      acc
+      
+      return acc
+    end
 
     def add_product(opts = {})
       res = post "#{resource_path}/#{id}/products", :body => opts
